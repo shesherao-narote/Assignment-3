9b019268bd3d5d71eef46da6e820fed28cf52711
Shashi <shashinarote97@gmail.com>
2024-12-31 17:30:42 +0530

    SecurityConfig created

---
+++ src/main/java/com/opticalarc/secure_web_app/configuration/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.opticalarc.secure_web_app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests((req)->
+                req
+                .requestMatchers("user/all").permitAll()
+                .anyRequest().authenticated()
+        ).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
